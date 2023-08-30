01e99d9b6744d2a3a203f9e43bfdd862ddb6205c
paria.m7616 <paria.m7616@gmail.com>
2023-08-30 04:58:26 -0930

    // push for debugging in SampleViewModelTest.kt

--- app/src/main/java/msi/paria/trainingapp/SampleViewModel.kt
+++ app/src/main/java/msi/paria/trainingapp/SampleViewModel.kt
@@ -1,6 +1,7 @@
 package msi.paria.trainingapp
 
 import android.util.Patterns
+import androidx.core.util.PatternsCompat
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -10,15 +11,20 @@ class SampleViewModel : ViewModel() {
     private val _userEmail = MutableStateFlow("")
     val userEmail : StateFlow<String> = _userEmail
 
+    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
+
     private val _emailValidateState = MutableStateFlow(false)
     val emailValidateState : StateFlow<Boolean> = _emailValidateState
 
     fun validateEmail(){
-        _emailValidateState.value = Patterns.EMAIL_ADDRESS.matcher(_userEmail.value).matches()
+        //_emailValidateState.value = _userEmail.value.matches(emailRegex.toRegex())
+        _emailValidateState.value = PatternsCompat.EMAIL_ADDRESS.matcher(_userEmail.value).matches()
+    }
+    fun onEmailChanged(email: String){
+        _userEmail.value = email
     }
 
-    fun onSubmitButtonClicked(email: String){
-        _userEmail.value = email
+    fun onSubmitButtonClicked(){
         validateEmail()
     }
 }
--- app/src/test/java/msi/paria/trainingapp/SampleViewModelTest.kt
+++ app/src/test/java/msi/paria/trainingapp/SampleViewModelTest.kt
@@ -6,8 +6,41 @@ class SampleViewModelTest {
     @Test
     fun validate_email_isCorrect(){
         val sampleViewModel = SampleViewModel()
-        val expected = true
         val email = "paria.m7616@gmail.com"
-        assert(expected) { sampleViewModel.onSubmitButtonClicked(email) }
+        sampleViewModel.onEmailChanged(email)
+        sampleViewModel.onSubmitButtonClicked()
+        assert(true) { sampleViewModel.emailValidateState.value }
     }
+
+    @Test
+    fun email_isWrong(){
+        val sampleViewModel = SampleViewModel()
+        val email = "paria."
+        sampleViewModel.onEmailChanged(email)
+        sampleViewModel.onSubmitButtonClicked()
+        assert(false) { sampleViewModel.emailValidateState.value }
+    }
+    @Test
+    fun email_isEmpty(){
+        val sampleViewModel = SampleViewModel()
+        val email = ""
+        sampleViewModel.onEmailChanged(email)
+        sampleViewModel.onSubmitButtonClicked()
+        assert(false) { sampleViewModel.emailValidateState.value }
+    }
+    @Test
+    fun email_isCorrect_button_not_clicked(){
+        val sampleViewModel = SampleViewModel()
+        val email = "paria.m7616@gmail.com"
+        sampleViewModel.onEmailChanged(email)
+        assert(false) { sampleViewModel.emailValidateState.value }
+    }
+    @Test
+    fun email_isCorrect_email_changed_not_received(){
+        val sampleViewModel = SampleViewModel()
+        val email = "paria.m7616@gmail.com"
+        sampleViewModel.onSubmitButtonClicked()
+        assert(false) { sampleViewModel.emailValidateState.value }
+    }
+
 }
