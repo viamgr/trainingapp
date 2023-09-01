5f12f3a4bade2fc039a632d4a50ccbfcf1bf7155
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-09-01 19:03:41 +0330

    Add missing dependency Add first textview with compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        compileSdkPreview = "UpsideDownCake"
+
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -34,7 +37,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 
     packaging {
@@ -42,13 +45,28 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+
 }
 
 dependencies {
-    implementation ("com.google.android.material:material:1.9.0")
+
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+
+
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation ("androidx.compose.material3:material3:1.1.1")
+
     implementation ("androidx.compose.ui:ui:1.5.0")
-    implementation ("androidx.compose.ui:ui-graphics:1.5.0")
     implementation ("androidx.compose.ui:ui-tooling-preview:1.5.0")
+    implementation ("androidx.compose.ui:ui-graphics:1.5.0")
 
 
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/msi/paria/trainingapp/MainActivity.kt
+++ app/src/main/java/msi/paria/trainingapp/MainActivity.kt
@@ -2,10 +2,33 @@ package msi.paria.trainingapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.Modifier
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+                textView()
+            }
+        }
+
     }
+
+
+    @Composable
+    fun textView() {
+        Text("Hello world")
+    }
+
+
 }
 
