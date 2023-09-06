3c8b159d9d9366271eb84aed3af46afb5edf1d5c
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-09-06 20:46:43 +0330

    Add preview ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation ("androidx.activity:activity-compose:1.7.2")
     implementation ("androidx.compose.material3:material3:1.1.1")
 
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
     implementation ("androidx.compose.ui:ui:1.5.0")
     implementation ("androidx.compose.ui:ui-tooling-preview:1.5.0")
     implementation ("androidx.compose.ui:ui-graphics:1.5.0")
--- app/src/main/java/msi/paria/trainingapp/MainActivity.kt
+++ app/src/main/java/msi/paria/trainingapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package msi.paria.trainingapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.Modifier
 import androidx.activity.compose.setContent
@@ -19,8 +18,11 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
@@ -33,12 +35,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContent {
-            Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
-                view()
-            }
-        }
 
         viewModelImpl = object : MainViewModelImpl {
             override fun setEmail(email: String) {
@@ -57,28 +53,46 @@ class MainActivity : ComponentActivity() {
             override fun onSubmitClicked() {}
         }
 
+        val state by viewModelImpl.getPageState()
 
+
+        setContent {
+            Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+                PageView(state)
+            }
+        }
     }
 
 
     @Composable
-    fun view() {
-        val state by viewModelImpl.getPageState()
-
+    fun PageView(pageState: MainPageState ) {
         Column(
             Modifier.padding(all = 16.dp),
             verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TextField(state.email, onValueChange = {
+            TextField(pageState.email, onValueChange = {
                 viewModelImpl.setEmail(it)
-            }, modifier = Modifier.fillMaxWidth(), isError = !state.isEmailCorrect)
+            }, modifier = Modifier.fillMaxWidth(), isError = !pageState.isEmailCorrect)
             Button(modifier = Modifier.fillMaxWidth(),
                 onClick = {
                     viewModelImpl.onSubmitClicked()
                 }, content = {
-                    Text(getString(R.string.check))
+                    Text(stringResource(R.string.check))
                 })
         }
+    }
+
+
+    @Preview
+    @Composable
+    fun errorPageView() {
+        PageView(pageState = MainPageState("invalid data" , false))
+    }
+
+    @Preview
+    @Composable
+    fun correctPageView() {
+        PageView(pageState = MainPageState("a@b.com" , true))
     }
 
 
