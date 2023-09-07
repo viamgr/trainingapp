d1d3e2cfc4bf5949214102290983bc554c707921
paria.m7616 <paria.m7616@gmail.com>
2023-09-07 02:38:37 -0930

    // delete jitpack maven from settings.gradle.kts that used for Faker Library(deleted)
    // delete empty lines from build.gradle(app)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven ( "https://jitpack.io")
-
     }
 }
 dependencyResolutionManagement {
@@ -12,8 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven ( "https://jitpack.io")
-
     }
 }
 
