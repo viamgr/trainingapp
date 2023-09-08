072d9d1e282d2be36fa7d218c35db7222cb6462c
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-09-08 22:29:35 +0330

    Remove extra classes Divided functions for single responsibility

--- app/src/main/java/msi/paria/trainingapp/pages/main/MainActivity.kt
+++ app/src/main/java/msi/paria/trainingapp/pages/main/MainActivity.kt
@@ -5,44 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import msi.paria.trainingapp.pages.main.iface.MainViewModelContract
 import msi.paria.trainingapp.pages.main.state.MainPageState
 import msi.paria.trainingapp.pages.main.view.MainPageView
 
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var viewModelImpl: MainViewModelContract
-
-    private val _state = mutableStateOf(MainPageState())
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModelImpl = object : MainViewModelContract {
-            override fun setEmail(email: String) {
-                _state.value = _state.value.copy(email, email.contains("@"))
-            }
-
-            override fun getEmail(): String {
-                return _state.value.email
-            }
-
-            override fun getPageState(): State<MainPageState> {
-                return _state
-            }
-
-            override fun onSubmitClicked() {}
-        }
+        val samplePageState = mutableStateOf(MainPageState())
 
         setContent {
             Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
-                MainPageView(mainPagePresenter = viewModelImpl)
+                MainPageView(pageState = samplePageState, setEmail = {
+                    samplePageState.value = samplePageState.value.copy(it, it.contains("@"))
+                }, onSubmitButtonClick = {})
             }
         }
     }
--- app/src/main/java/msi/paria/trainingapp/pages/main/view/MainPageView.kt
+++ app/src/main/java/msi/paria/trainingapp/pages/main/view/MainPageView.kt
@@ -9,63 +9,57 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import msi.paria.trainingapp.R
-import msi.paria.trainingapp.pages.main.iface.MainViewModelContract
 import msi.paria.trainingapp.pages.main.state.MainPageState
 
 
 @Composable
-fun MainPageView(mainPagePresenter: MainViewModelContract) {
-    val pageState = mainPagePresenter.getPageState()
+fun MainPageView(pageState: State<MainPageState>, setEmail: (newEmail: String) -> Unit, onSubmitButtonClick: () -> Unit) {
     Column(
         Modifier.padding(all = 16.dp),
         verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TextField(pageState.value.email, onValueChange = {
-            mainPagePresenter.setEmail(it)
-        }, modifier = Modifier.fillMaxWidth(), isError = !pageState.value.isEmailCorrect)
-        Button(modifier = Modifier.fillMaxWidth(),
-            onClick = {
-                mainPagePresenter.onSubmitClicked()
-            }, content = {
-                Text(stringResource(R.string.check))
-            })
+        TextFiledView(pageState.value.email, pageState.value.isEmailCorrect, setEmail)
+        CheckButtonView(onSubmitButtonClick)
     }
 }
 
-
-@Preview
 @Composable
-fun ErrorPageView() {
-    MainPageView(getSimplePresenter(MainPageState("invalid data", false)))
+fun TextFiledView(email: String, isEmailCorrect: Boolean, setEmail: (newEmail: String) -> Unit) {
+    TextField(email, onValueChange = {
+        setEmail(it)
+    }, modifier = Modifier.fillMaxWidth(), isError = !isEmailCorrect)
+}
+
+@Composable
+fun CheckButtonView(onSubmitButtonClick: () -> Unit) {
+    Button(modifier = Modifier.fillMaxWidth(),
+        onClick = {
+            onSubmitButtonClick.invoke()
+        }, content = {
+            Text(stringResource(R.string.check))
+        })
 }
 
 @Preview
 @Composable
-fun CorrectPageView() {
-    MainPageView(getSimplePresenter(MainPageState("a@b.com", true)))
+fun ErrorTextFiledView() {
+    TextFiledView("aaaa", false, {})
 }
 
-fun getSimplePresenter(pageState: MainPageState): MainViewModelContract {
-    return object : MainViewModelContract {
-        override fun setEmail(email: String) {
-        }
+@Preview
+@Composable
+fun CorrectTextFiledView() {
+    TextFiledView("a@b.com", true, {})
+}
 
-        override fun getEmail(): String {
-            return pageState.email
-        }
-
-        override fun getPageState(): State<MainPageState> {
-            return mutableStateOf(pageState)
-        }
-
-
-        override fun onSubmitClicked() {}
-    }
+@Preview
+@Composable
+fun CheckButtonPreview() {
+    CheckButtonView {}
 }
