ed6c5c052b156638151b04491a1c67fe226cee5a
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-09-08 14:44:15 +0330

    Remove empty space and clean up gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,10 +32,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
@@ -53,13 +55,11 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
-
 }
 
 dependencies {
 
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-
 
     implementation ("androidx.activity:activity-compose:1.7.2")
     implementation ("androidx.compose.material3:material3:1.1.1")
@@ -68,7 +68,6 @@ dependencies {
     implementation ("androidx.compose.ui:ui:1.5.0")
     implementation ("androidx.compose.ui:ui-tooling-preview:1.5.0")
     implementation ("androidx.compose.ui:ui-graphics:1.5.0")
-
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
