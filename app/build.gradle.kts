41d2ac72a286be712a977aea3c4dcc84005eab24
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-09-11 18:38:18 +0330

    Add compose test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha05")
+
+
+
 }
