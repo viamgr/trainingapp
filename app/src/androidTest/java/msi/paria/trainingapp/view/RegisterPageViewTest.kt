ba4aa9de96b9e132a5d40dcf2ba5305f8b137945
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-09-15 10:18:04 +0330

    Change test function name

--- app/src/androidTest/java/msi/paria/trainingapp/view/RegisterPageViewTest.kt
+++ app/src/androidTest/java/msi/paria/trainingapp/view/RegisterPageViewTest.kt
@@ -1,10 +1,5 @@
 package msi.paria.trainingapp.view
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
@@ -15,7 +10,6 @@ import junit.framework.TestCase
 import msi.paria.trainingapp.pages.main.view.CheckButtonView
 import msi.paria.trainingapp.pages.main.view.RegisterPageView
 import msi.paria.trainingapp.pages.main.view.TextFiledView
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,23 +19,18 @@ class RegisterPageViewTest {
 
     @get: Rule
     val composeTestRule = createComposeRule()
-
-    @Before
-    fun setUp() {
-    }
+    
 
     @Test
-    fun GivenView_WhenViewCreate_ThenMustFindAllViews() {
+    fun givenView_WhenViewCreate_ThenShouldAllViewDisplayed() {
 
         composeTestRule.setContent {
-            Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
-                RegisterPageView("",
-                    false,
-                    onEmailChanged = {
-                    },
-                    onSubmitButtonClick = {
-                    })
-            }
+            RegisterPageView("",
+                false,
+                onEmailChanged = {
+                },
+                onSubmitButtonClick = {
+                })
         }
 
         composeTestRule.onNodeWithTag("textFiled").assertExists()
@@ -49,13 +38,13 @@ class RegisterPageViewTest {
     }
 
     @Test
-    fun GivenView_WhenViewCreate_ThenMustFindTextField() {
+    fun givenView_WhenViewCreate_ThenShouldTextFieldDisplayed() {
         showSampleTextFiled("", false) {}
         composeTestRule.onNodeWithTag("textFiled").assertExists()
     }
 
     @Test
-    fun GivenEmail_WhenSendEmail_ThenTextFiledTextMustBeSame() {
+    fun givenEmail_WhenCreateTextFiledWithEmail_ThenTextFiledTextShouldBeSameAsGivenEmail() {
         val email = "email"
 
         showSampleTextFiled(email, false) {}
@@ -64,25 +53,24 @@ class RegisterPageViewTest {
     }
 
     @Test
-    fun GivenEmail_WhenChangeEmail_ThenTextFiledTextMustBeSame() {
+    fun givenEmail_WhenEmailChanged_ThenTextFiledTextShouldBeSameAsEmail() {
         var email = ""
 
         showSampleTextFiled("", false) { email = it }
 
         composeTestRule.onNodeWithTag("textFiled").performTextInput("Hi")
-        composeTestRule.onNodeWithTag("textFiled").assertIsEnabled()
         TestCase.assertEquals(email, "Hi")
     }
 
     @Test
-    fun GivenView_WhenViewCreate_ThenMustFindCheckButton() {
+    fun givenView_WhenViewCreate_ThenShouldCheckButtonDisplayed() {
         showSampleCheckButton {}
 
         composeTestRule.onNodeWithTag("button").assertExists()
     }
 
     @Test
-    fun GivenView_WhenButtonClicked_ThenMustButtonClickBeTrue() {
+    fun givenView_WhenButtonClicked_ThenShouldButtonClicked() {
         var clicked = false
 
         showSampleCheckButton { clicked = true }
@@ -93,22 +81,18 @@ class RegisterPageViewTest {
 
     private fun showSampleTextFiled(email: String, isEmailCorrect: Boolean, onEmailChanged: (newEmail: String) -> Unit) {
         composeTestRule.setContent {
-            Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
-                TextFiledView(
-                    email, isEmailCorrect,
-                    onEmailChanged = onEmailChanged
-                )
-            }
+            TextFiledView(
+                email, isEmailCorrect,
+                onEmailChanged = onEmailChanged
+            )
         }
     }
 
     private fun showSampleCheckButton(onSubmitButtonClick: () -> Unit) {
         composeTestRule.setContent {
-            Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
-                CheckButtonView(
-                    onSubmitButtonClick = onSubmitButtonClick
-                )
-            }
+            CheckButtonView(
+                onSubmitButtonClick = onSubmitButtonClick
+            )
         }
     }
 
