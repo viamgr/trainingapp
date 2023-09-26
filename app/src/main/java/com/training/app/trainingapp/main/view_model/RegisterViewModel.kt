577927614ca7ad8bf61c401d506371c111474ca0
pariami <paria.m7616>
2023-09-26 09:33:20 -0930

    resolve conflict with main branch

--- app/src/main/java/com/training/app/trainingapp/main/view_model/RegisterViewModel.kt
+++ app/src/main/java/com/training/app/trainingapp/main/view_model/RegisterViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+
 @HiltViewModel
 class RegisterViewModel : ViewModel() {
 
