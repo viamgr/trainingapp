e7f69617fd2613fbd243255dab0e06937531871a
pariami <paria.m7616>
2023-09-27 02:36:10 -0930

    remove empty space from build.gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,24 +4,20 @@ plugins {
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
 }
-
 android {
     namespace = "com.training.app.trainingapp"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.training.app.trainingapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -31,60 +27,43 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
     }
-
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-
     buildFeatures {
         compose = true
     }
-
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
 }
 
 dependencies {
-
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-
-    implementation ("androidx.activity:activity-compose:1.7.2")
-    implementation ("androidx.compose.material3:material3:1.1.1")
-
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3:1.1.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
-    implementation ("androidx.compose.ui:ui:1.5.1")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.5.1")
-    implementation ("androidx.compose.ui:ui-graphics:1.5.1")
-
+    implementation("androidx.compose.ui:ui:1.5.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.1")
+    implementation("androidx.compose.ui:ui-graphics:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation("androidx.test.ext:junit-ktx:1.1.5")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha05")
-
-
-
-
     implementation("androidx.test.ext:junit-ktx:1.1.5")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha05")
-
-
-
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
