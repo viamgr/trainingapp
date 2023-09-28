3819f4b345930146733e2f56df3bb9e2507726fd
pariami <paria.m7616>
2023-09-28 05:48:43 -0930

    set correct space to build.gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,20 +4,26 @@ plugins {
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
 }
+
 android {
     namespace = "com.training.app.trainingapp"
     compileSdk = 34
+
     defaultConfig {
         applicationId = "com.training.app.trainingapp"
+
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
     }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -27,21 +33,26 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
@@ -59,14 +70,17 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
+
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha05")
     implementation("androidx.test.ext:junit-ktx:1.1.5")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha05")
+
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
+
 kapt {
     correctErrorTypes = true
 }
