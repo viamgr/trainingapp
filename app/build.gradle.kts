71ec936d5c10166df8d930f968e249ff15aba1a0
Paria Masoumi <paria.m7616@gmail.com>
2023-09-30 09:03:36 -0930

    21 setup hilt (#23)
    * Add compose dependency
    
    * Add build folder to git ignore
    
    * Add missing dependency
    Add first textview with compose
    
    * Add main activity view
    
    * Add preview ui
    
    * Cleanup code
    
    * Update kotlin version
    
    * Update .gitignore file
    
    * Remove empty space and clean up gradle file
    
    * Remove extra classes
    Divided functions for single responsibility
    
    * change setEmail to onEmailChanged
    
    * update change setEmail to onEmailChanged in MainActivity
    
    * Remove compileSdkPreview and change targetSdk
    
    * Remove pageState
    Rename viewModel
    
    * Rename MainPageView to RegisterpageView
    
    * Use primitive type in RegisterPageView
    
    * Rename setEmail to onEmailChanged
    
    * Use onSubmitButtonClicked
    
    * Rename main mainViewModel to registerViewModel
    
    * // Add Hilt dependencies and plugin to the project
    // Add Application class
    
    * Change SampleViewModelTest to RegisterViewModelTest
    
    * Add compose test dependencies
    
    * Add test tag to view to use in test cases
    
    * Create simple test for view
    
    * Add view test case
    
    * Change package name
    
    * Change package name
    
    * Change test function name
    
    * Add single line to email textfiled
    
    * Change package
    Split views to multiple classes
    Change test cases
    
    * Bug fixed
    change node expected id in RegisterPageViewTest
    
    * Remove test tag from function parameter
    
    * Store data from view model and send to view
    
    * Add TestTags class to store test tag ids in project
    
    * // Setup Hilt for ViewModel
    
    * resolve conflict with main branch
    
    * remove empty space from build.gradle file
    
    * set correct space to build.gradle file
    
    ---------
    
    Co-authored-by: sadegh1371 <sadegh.dehghani1992@gmail.com>
    Co-authored-by: pariami <paria.m7616>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -9,12 +11,14 @@ android {
 
     defaultConfig {
         applicationId = "com.training.app.trainingapp"
+
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -55,25 +59,28 @@ android {
 }
 
 dependencies {
-
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-
-    implementation ("androidx.activity:activity-compose:1.7.2")
-    implementation ("androidx.compose.material3:material3:1.1.1")
-
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3:1.1.1")
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
-    implementation ("androidx.compose.ui:ui:1.5.1")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.5.1")
-    implementation ("androidx.compose.ui:ui-graphics:1.5.1")
-
+    implementation("androidx.compose.ui:ui:1.5.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.1")
+    implementation("androidx.compose.ui:ui-graphics:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     implementation("androidx.test.ext:junit-ktx:1.1.5")
+
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha05")
+    implementation("androidx.test.ext:junit-ktx:1.1.5")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0-alpha05")
 
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+}
 
-
+kapt {
+    correctErrorTypes = true
 }
---
+++ app/src/main/java/com/training/app/trainingapp/core/TrainingAppApplication.kt
@@ -0,0 +1,8 @@
+package com.training.app.trainingapp.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TrainingAppApplication : Application() {
+}
