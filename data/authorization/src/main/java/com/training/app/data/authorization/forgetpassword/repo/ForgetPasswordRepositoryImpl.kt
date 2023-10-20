c3c677fbf9397b2626d1a2de1b058b692e32370a
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-10-20 13:51:23 +0330

    - Add Inject to repository impl

--- data/authorization/src/main/java/com/training/app/data/authorization/forgetpassword/repo/ForgetPasswordRepositoryImpl.kt
+++ data/authorization/src/main/java/com/training/app/data/authorization/forgetpassword/repo/ForgetPasswordRepositoryImpl.kt
@@ -4,8 +4,9 @@ import com.training.app.data.authorization.forgetpassword.dataprovider.ForgetPas
 import com.training.app.data.authorization.forgetpassword.dto.ForgetPasswordModelResponseAdapter
 import com.trainning.app.domain.model.ForgetPasswordResponse
 import com.trainning.app.domain.repository.ForgetPasswordRepository
+import javax.inject.Inject
 
-class ForgetPasswordRepositoryImpl constructor(private val dataProvider: ForgetPasswordDataProvider) : ForgetPasswordRepository {
+class ForgetPasswordRepositoryImpl @Inject constructor(private val dataProvider: ForgetPasswordDataProvider) : ForgetPasswordRepository {
     override suspend fun forgetPassword(email: String): ForgetPasswordResponse {
         return ForgetPasswordModelResponseAdapter(dataProvider.forgetPassword(email)).map()
     }
