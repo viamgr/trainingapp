a36d30c8423634dfa047f3c26178da458daa7b4c
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-11-16 22:34:41 +0330

    Change ForgetPasswordDataProviderImpl to internal class

--- data/authorization/src/main/java/com/training/app/data/authorization/forgetpassword/dto/ForgetPasswordModelResponseAdapter.kt
+++ data/authorization/src/main/java/com/training/app/data/authorization/forgetpassword/dto/ForgetPasswordModelResponseAdapter.kt
@@ -2,7 +2,7 @@ package com.training.app.data.authorization.forgetpassword.dto
 
 import com.trainning.app.domain.model.ForgetPasswordResponse
 
-class ForgetPasswordModelResponseAdapter constructor(val modelResponse: ForgetPasswordModelResponse) {
+internal class ForgetPasswordModelResponseAdapter constructor(val modelResponse: ForgetPasswordModelResponse) {
 
     fun map(): ForgetPasswordResponse {
         return ForgetPasswordResponse(modelResponse.isSuccess)
