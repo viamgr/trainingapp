a0af0dbfd07a3ad39ee46b594584d1e900d443e2
sadegh1371 <sadegh.dehghani1992@gmail.com>
2023-12-10 21:37:24 +0330

    Add coroutines rule

--- app/src/test/java/com/training/trainingapp/view_model/SignUpViewModelTest.kt
+++ app/src/test/java/com/training/trainingapp/view_model/SignUpViewModelTest.kt
@@ -3,33 +3,35 @@ package com.training.trainingapp.view_model
 import app.cash.turbine.test
 import com.training.app.trainingapp.main.state.base.PageState
 import com.training.app.trainingapp.main.view_model.signup.SignUpViewModel
+import com.training.trainingapp.MainCoroutineRule
 import com.trainning.app.domain.model.SignUpResponse
 import com.trainning.app.domain.usecase.SignUpViewUseCase
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
 class SignUpViewModelTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
     lateinit var sampleViewModel: SignUpViewModel
 
     val signUpViewUseCase = mockk<SignUpViewUseCase>()
 
     @Before
     fun initValues() {
-        Dispatchers.setMain(UnconfinedTestDispatcher())
         sampleViewModel = SignUpViewModel(signUpViewUseCase)
     }
 
@@ -110,12 +112,6 @@ class SignUpViewModelTest {
             assertEquals(PageState.LOADING, awaitItem().pageState)
             assertEquals(PageState.FAILED, awaitItem().pageState)
         }
-    }
-
-
-    @After
-    fun close() {
-        Dispatchers.shutdown()
     }
 
 }
